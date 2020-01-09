0d90a03bcae0e27cd43a8fa29f035cefe85ba71d
Alexander Ryabtsev <hazzelgrouse@gmail.com>
2020-01-09 18:00:31 +0300

    Add SQS to the state machine

--- services/random-state-machine/handler.ts
+++ services/random-state-machine/handler.ts
@@ -1,5 +1,14 @@
+import { StepFunctions } from 'aws-sdk'
+import { SQSEvent } from 'aws-lambda'
+
 interface GenerateRandomNumberInput {
   threshold: number
+  max: number
+}
+
+interface WaitForTaskTokenSQSMessageBody {
+  Message: string | number
+  TaskToken: string
 }
 
 class WrongRandomNumber extends Error {
@@ -10,8 +19,8 @@ class WrongRandomNumber extends Error {
 }
 
 const generateRandomNumber = async (input: GenerateRandomNumberInput): Promise<number> => {
-  const { threshold } = input
-  const value = Math.floor(Math.random() * Math.floor(10))
+  const { threshold, max } = input
+  const value = Math.floor(Math.random() * Math.floor(max))
 
   if (value < threshold) {
     throw new WrongRandomNumber(`Value must be >= ${threshold}, generated value is ${value}`)
@@ -20,6 +29,36 @@ const generateRandomNumber = async (input: GenerateRandomNumberInput): Promise<n
   return value
 }
 
+const handleOneDigitNumber = async (event: SQSEvent): Promise<void> => {
+  const stepFunctions = new StepFunctions()
+  const body = JSON.parse(event.Records[0].body) as WaitForTaskTokenSQSMessageBody
+
+  const params: StepFunctions.Types.SendTaskSuccessInput = {
+    output: JSON.stringify({
+      message: `Value ${body.Message} is handled successfully.`,
+    }),
+    taskToken: body.TaskToken,
+  }
+
+  await stepFunctions.sendTaskSuccess(params).promise()
+}
+
+const handleTwoDigitNumber = async (event: SQSEvent): Promise<void> => {
+  const stepFunctions = new StepFunctions()
+  const body = JSON.parse(event.Records[0].body) as WaitForTaskTokenSQSMessageBody
+
+  const params: StepFunctions.Types.SendTaskSuccessInput = {
+    output: JSON.stringify({
+      message: `Value ${body.Message} is handled successfully.`,
+    }),
+    taskToken: body.TaskToken,
+  }
+
+  await stepFunctions.sendTaskSuccess(params).promise()
+}
+
 export {
   generateRandomNumber,
+  handleOneDigitNumber,
+  handleTwoDigitNumber,
 }
